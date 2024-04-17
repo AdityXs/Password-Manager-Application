a1ddd97fed48dd0f451d83381d6e99268f912bc6
Aditya Singh <141888894+AdityXs@users.noreply.github.com>
2024-04-17 18:52:16 +1000

    Master Password Creation Page V0.2
    Added New Functionalities
    
    > Made a working 'confirmation' button that checks if the string input in the entry fields are the same or not, currently the output is being sent in the terminal which I will need to instead put in the application screen. 
    
    > Made a working 'show/hide' button for the passwords that will be inputted in the entry field, there is a button respective for each entry field, the button is a toggle button which just hides the password string in asterisk's or "*" 
    
    Fixes needed to be made
    
    > The 'show/hide' buttons for the passwords/entry fields need to be placed to the right of the entry field (aesthetic issue)
    
    > The prefilled text in the entry field can be hidden using the 'show/hide' buttons, essentially the prefilled out text counts as string value in the entry field, need to be made transparent and should not be counted as preexisting content for the page (logic issue)
    
    > The confirmation message for whether the passwords match or not should be in the app, currently the output messages can only be seen in the terminal (easy issue, can do later)

--- Password Manager Assignment.py
+++ Password Manager Assignment.py
@@ -21,25 +21,39 @@ instruction_label.pack()
 def entry_focus_in(event):
     if entry_1.get() == "Choose Password":
         entry_1.delete(0, 'end')
-        entry_1.config(fg="gray") #what is this??
+        entry_1.configure(fg_color="gray") #what is this??
 
 
 def entry_focus_out(event):
     if entry_1.get() == "":
         entry_1.insert(0, "Choose Password")
-        entry_1.config(fg='gray')
+        entry_1.configure(fg_color='gray')
 
 def entry_focus_in2(event):
     if entry_2.get() == "Confirm Password":
         entry_2.delete(0, 'end')
-        entry_2.config(fg="gray") #what is this??
+        entry_2.configure(fg_color="gray") #what is this??
 
 
 def entry_focus_out2(event):
     if entry_2.get() == "":
         entry_2.insert(0, "Confirm Password")
-        entry_2.config(fg='gray')
+        entry_2.configure(fg_color='gray')
 
+#Password Validator... Need to work on this later but core mechanic works for now
+def check_passwords():
+    password1 = entry_1.get()
+    password2 = entry_2.get()
+    if password1 == password2:
+        print("Passwords match!")
+    else:
+        print("Passwords do not match!")
+
+def toggle_password_visibility(entry):
+    if entry.cget('show') == '':
+        entry.configure(show='*')
+    else:
+        entry.configure(show='')
 
 #Entry Fields
 
@@ -49,6 +63,11 @@ entry_1.insert(0,"Choose Password")
 entry_1.bind("<FocusIn>", entry_focus_in)
 entry_1.bind("<FocusOut>", entry_focus_out)
 
+#Hide Password Button
+eye_button_1 = customtkinter.CTkButton(root, text="Show/Hide", command=lambda: toggle_password_visibility(entry_1))
+eye_button_1.pack(pady=10)
+
+#Entry Fields 2
 
 entry_2 = customtkinter.CTkEntry(root, width=170, height=20)
 entry_2.pack(pady=30)
@@ -56,7 +75,12 @@ entry_2.insert(0,"Confirm Password")
 entry_2.bind("<FocusIn>", entry_focus_in2)
 entry_2.bind("<FocusOut>", entry_focus_out2)
 
+#Hide Password button
+eye_button_2 = customtkinter.CTkButton(root, text="Show/Hide", command=lambda: toggle_password_visibility(entry_2))
+eye_button_2.pack(pady=10)
 
-checkbutton = customtkinter.CTkButton(root, text = "Check")
+#Have to make the validation message appear on window 
+checkbutton = customtkinter.CTkButton(root, text="Confirm", command=check_passwords)
 checkbutton.pack(pady = 30)
+
 root.mainloop()
